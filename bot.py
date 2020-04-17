b03ed932ebe463f224393c0bf7f8c40246e8b88b
Kumar, Vinay (SDG) <Vinay_Kumar@intuit.com>
2020-04-17 16:50:07 -0700

    Support for multiple guessers in one invocation

--- bot.py
+++ bot.py
@@ -1,5 +1,3 @@
-import sys
-
 answers = ['Dykstra', 'Backman', 'Hernandez', 'Carter', 'Strawberry', 'Foster', 'Johnson', 'Santana', 'Gooden']
 
 answered_by = {}
@@ -8,20 +6,22 @@ for answer in answers:
 
 incorrect_guesses = []
 
-guesses = sys.argv[2:]
-guesser = sys.argv[1]
-print(f'{guesser}\'s Guesses:')
-for guess in guesses:
-    if guess in answers:
-        if answered_by[guess]:
-            print(f"{guess}: Correct, but already named by {answered_by[guess]}")
+responses = [{'guesser': 'SJ', 'guesses': ['Foster', 'Santana']}, {'guesser': 'Harold', 'guesses': ['Coleman', 'Gooden']}]
+for response in responses:
+    guesser = response['guesser']
+    guesses = response['guesses']
+    print(f'{guesser}\'s Guesses:')
+    for guess in guesses:
+        if guess in answers:
+            if answered_by[guess]:
+                print(f"{guess}: Correct, but already named by {answered_by[guess]}")
+            else:
+                print(f"{guess}: DING!  Correct")
+                answered_by[guess] = guesser
         else:
-            print(f"{guess}: DING!  Correct")
-            answered_by[guess] = guesser
-    else:
-        print(f"{guess}: Incorrect")
-        if guess not in incorrect_guesses:
-            incorrect_guesses.append(guess)
+            print(f"{guess}: Incorrect")
+            if guess not in incorrect_guesses:
+                incorrect_guesses.append(guess)
 
 
 print()
