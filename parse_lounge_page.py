9feb8744bf5af3a98c473940befc830c57226d68
Kumar, Vinay (SDG) <Vinay_Kumar@intuit.com>
2020-04-18 18:56:55 -0700

    Code simplification/readability

--- parse_lounge_page.py
+++ parse_lounge_page.py
@@ -7,20 +7,27 @@ def get_lounge_posts(fp):
         yield post
 
 
-with open("fixtures/lounge_6329_600.html") as fp:
-    for post in get_lounge_posts(fp):
-        username = post.parent.parent.a.text
-        post_time = post.parent.parent.parent.find_all('td')[1].text[8:]
+def get_guesses():
+    with open("fixtures/lounge_6329_600.html") as fp:
+        for post in get_lounge_posts(fp):
+            username = post.parent.parent.a.text
+            post_time = post.parent.parent.parent.find_all('td')[1].text[8:]
 
-        if '#LoungeTrivia_86Mets' not in post.text or 'Harold' in username:
-            continue
+            if '#LoungeTrivia_86Mets' not in post.text or 'Harold' in username:
+                continue
 
-        # The guesses are in the text that follows the quote
-        # So find the quote, and then move to the following siblings
-        cursor = post.blockquote
-        guesses = []
-        while cursor and cursor.find_next_sibling('p'):
-            cursor = cursor.find_next_sibling('p')
-            guesses += cursor.text.split("\n")  # guesses may be in multiple lines in one <p>
+            # The guesses are in the text that follows the quote
+            # So find the quote, and then move to the following siblings
+            cursor = post.blockquote
+            guesses = []
+            # guesses might be in separate <p>s or in separate lines in one <p>
+            while cursor and cursor.find_next_sibling('p'):
+                cursor = cursor.find_next_sibling('p')
+                guesses += cursor.text.split("\n")
 
+            yield(post_time, username, guesses)
+
+
+if __name__ == "__main__":
+    for (post_time, username, guesses) in get_guesses():
         print(f"{post_time} {username}: {guesses}")
