edf014cd27ff463af1da4eb4b9805100547a486a
Kumar, Vinay (SDG) <Vinay_Kumar@intuit.com>
2020-04-19 16:31:05 -0700

    Write responses out to csv

--- parse_lounge_page.py
+++ parse_lounge_page.py
@@ -1,3 +1,4 @@
+import csv
 from bs4 import BeautifulSoup
 
 
@@ -10,24 +11,33 @@ def get_lounge_posts(fp):
 def get_guesses(fp):
     for post in get_lounge_posts(fp):
         username = post.parent.parent.a.text
-        post_time = post.parent.parent.parent.find_all('td')[1].text[8:]
 
         if '#LoungeTrivia_86Mets' not in post.text or 'Harold' in username:
             continue
 
+        response = {}
+        response['username'] = username
+        response['time'] = post.parent.parent.parent.find_all('td')[1].text[8:]
+        response['num'] = post.parent.parent.parent.find_all('a')[3].text[2:]
+        response['id'] = post.parent.parent.parent.find_all('a')[3]['name']
+
         # The guesses are in the text that follows the quote
         # So find the quote, and then move to the following siblings
         cursor = post.blockquote
-        guesses = []
+        response['guesses'] = []
         # guesses might be in separate <p>s or in separate lines in one <p>
         while cursor and cursor.find_next_sibling('p'):
             cursor = cursor.find_next_sibling('p')
-            guesses += cursor.text.split("\n")
+            response['guesses'] += cursor.text.split("\n")
 
-        yield(post_time, username, guesses)
+        yield(response)
 
 
 if __name__ == "__main__":
     with open("fixtures/lounge_6329_600.html") as fp:
-        for (post_time, username, guesses) in get_guesses(fp):
-            print(f"{post_time} {username}: {guesses}")
+        with open('guesses_6329_600.csv', 'w', newline='') as csvfile:
+            fieldnames = ['time', 'num', 'id', 'username', 'guesses']
+            writer = csv.DictWriter(csvfile, fieldnames)
+            for response in get_guesses(fp):
+                print(response)
+                writer.writerow(response)
