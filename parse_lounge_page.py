5462dea5b803b159e94823fe32701287894bea79
Kumar, Vinay (SDG) <Vinay_Kumar@intuit.com>
2020-04-18 17:30:06 -0700

    Code simplification (aka guards are our friends)

--- parse_lounge_page.py
+++ parse_lounge_page.py
@@ -9,14 +9,18 @@ def get_lounge_posts(fp):
 
 with open("fixtures/lounge_6329_600.html") as fp:
     for post in get_lounge_posts(fp):
-        if post.text.find('#LoungeTrivia_86Mets') > -1:
-            username = post.parent.parent.a.text
-            if username.find('Harold') == -1:
-                post_time = post.parent.parent.parent.find_all('td')[1].text
-                cursor = post.blockquote
-                guesses = []
-                while cursor and cursor.find_next_sibling('p'):
-                    cursor = cursor.find_next_sibling('p')
-                    guesses += cursor.text.split("\n")
+        if '#LoungeTrivia_86Mets' not in post.text:
+            continue
 
-                print(username, post_time, guesses)
+        username = post.parent.parent.a.text
+        if 'Harold' in username:
+            continue
+
+        post_time = post.parent.parent.parent.find_all('td')[1].text
+        cursor = post.blockquote
+        guesses = []
+        while cursor and cursor.find_next_sibling('p'):
+            cursor = cursor.find_next_sibling('p')
+            guesses += cursor.text.split("\n")
+
+        print(username, post_time, guesses)
